11e3688810d0de978d8e749add171704ce69a20d
Alfin <alfinthumbanath@gmail.com>
2024-06-21 02:48:05 +0530

    code cleaned

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_assignment/data/models/location_response.dart';
 import 'package:flutter_assignment/screen/location_details_screen.dart';
+import 'package:flutter_assignment/screen/widgets/common_app_bar.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
@@ -20,6 +21,138 @@ class MyHomePageState extends State<MyHomePage> {
 
   final String title;
   TextEditingController editingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: CommonAppBar(title: title), body: HomePageBody());
+  }
+}
+
+class HomePageBody extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomePageBodyState();
+}
+
+class HomePageBodyState extends State<HomePageBody> {
+  TextEditingController searchTextEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<LocationProvider>(
+        builder: (context, dataProvider, child) => Center(
+            child: dataProvider.isLoading
+                ? CircularProgressIndicator(
+                    color: Theme.of(context).colorScheme.inversePrimary)
+                : dataProvider.errorMessage.isNotEmpty
+                    ? Text(dataProvider.errorMessage)
+                    : dataProvider.locations != null
+                        ? Padding(
+                            padding: const EdgeInsets.only(
+                                right: 5, left: 5, top: 12, bottom: 12),
+                            child: Column(children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  children: <Widget>[
+                                    Expanded(
+                                      child: TextField(
+                                        controller: searchTextEditingController,
+                                        decoration: const InputDecoration(
+                                            contentPadding: EdgeInsets.only(
+                                                left: 5,
+                                                top: 12,
+                                                right: 2,
+                                                bottom: 12),
+                                            focusedBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                  color: Colors.black,
+                                                  width: 2.0),
+                                            ),
+                                            enabledBorder: OutlineInputBorder(
+                                              borderSide: BorderSide(
+                                                  color: Colors.black54,
+                                                  width: 2.0),
+                                            ),
+                                            hintText: 'Search here...'),
+                                        onChanged: (query) {
+                                          dataProvider.applySearchQuery(query);
+                                          dataProvider.fetchLocationsResult();
+                                        },
+                                      ),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        dataProvider
+                                            .applyFilter(locationFilterAll);
+                                        dataProvider.fetchLocationsResult();
+                                      },
+                                      style: TextButton.styleFrom(
+                                          padding: EdgeInsets.zero,
+                                          minimumSize: Size(30, 30),
+                                          tapTargetSize:
+                                              MaterialTapTargetSize.padded,
+                                          alignment: Alignment.center),
+                                      child: Text('All'),
+                                    ),
+                                    IconButton(
+                                        icon: const Icon(
+                                            Icons.filter_list_outlined),
+                                        onPressed: () {
+                                          _showFilters(context, dataProvider);
+                                        }),
+                                  ],
+                                ),
+                              ),
+                              if (dataProvider.filteredLocations?.isEmpty ==
+                                  true)
+                                const Center(
+                                  child: Text('No result found!'),
+                                )
+                              else
+                                Expanded(
+                                    child: ListView.builder(
+                                        itemCount: dataProvider
+                                            .filteredLocations?.length,
+                                        itemBuilder: (context, index) {
+                                          Location? location =
+                                              (dataProvider.filteredLocations ??
+                                                  [])[index];
+                                          return Column(children: [
+                                            ListTile(
+                                              title: Text(
+                                                location?.location ?? "",
+                                                style: GoogleFonts.poppins(
+                                                    fontSize: 18,
+                                                    color: Colors.black,
+                                                    fontWeight:
+                                                        FontWeight.w500),
+                                              ),
+                                              subtitle: Text(
+                                                '${location?.geo} - ${location?.area}',
+                                                style: GoogleFonts.poppins(
+                                                    fontSize: 14,
+                                                    color: Colors.black54,
+                                                    fontWeight:
+                                                        FontWeight.w500),
+                                              ),
+                                              trailing: const Icon(Icons
+                                                  .keyboard_arrow_right_rounded),
+                                              onTap: () {
+                                                Navigator.push(
+                                                    context,
+                                                    MaterialPageRoute(
+                                                        builder: (context) =>
+                                                            LocationDetailsPage(
+                                                                location:
+                                                                    location!)));
+                                              },
+                                            ),
+                                            const Divider()
+                                          ]);
+                                        })),
+                            ]))
+                        : const Center(child: Text('No data found!'))));
+  }
 
   Future<void> _showFilters(
       BuildContext context, LocationProvider dataProvider) async {
@@ -62,142 +195,5 @@ class MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    TextEditingController searchTextEditingController = TextEditingController();
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text(title,
-              style: GoogleFonts.poppins(
-                  color: Colors.white, fontWeight: FontWeight.w500)),
-          actions: [
-            Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
-                child: Image.asset('assets/images/tcs_logo.png',
-                    fit: BoxFit.cover))
-          ],
-        ),
-        body: Consumer<LocationProvider>(
-            builder: (context, dataProvider, child) => Center(
-                child: dataProvider.isLoading
-                    ? CircularProgressIndicator(
-                        color: Theme.of(context).colorScheme.inversePrimary)
-                    : dataProvider.errorMessage.isNotEmpty
-                        ? Text(dataProvider.errorMessage)
-                        : dataProvider.locations != null
-                            ? Padding(
-                                padding:
-                                    const EdgeInsets.only(right: 16, left: 16),
-                                child: Column(children: [
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Row(
-                                      children: <Widget>[
-                                        Expanded(
-                                          child: TextField(
-                                            controller:
-                                                searchTextEditingController,
-                                            decoration: const InputDecoration(
-                                                contentPadding:
-                                                    EdgeInsets.all(10),
-                                                focusedBorder:
-                                                    OutlineInputBorder(
-                                                  borderSide: BorderSide(
-                                                      color: Colors.black,
-                                                      width: 2.0),
-                                                ),
-                                                enabledBorder:
-                                                    OutlineInputBorder(
-                                                  borderSide: BorderSide(
-                                                      color: Colors.black54,
-                                                      width: 2.0),
-                                                ),
-                                                hintText: 'Search here...'),
-                                            onChanged: (query) {
-                                              dataProvider
-                                                  .applySearchQuery(query);
-                                              dataProvider
-                                                  .fetchLocationsResult();
-                                            },
-                                          ),
-                                        ),
-                                        TextButton(
-                                          onPressed: () {
-                                            dataProvider
-                                                .applyFilter(locationFilterAll);
-                                            dataProvider.fetchLocationsResult();
-                                          },
-                                          style: TextButton.styleFrom(
-                                              padding: EdgeInsets.zero,
-                                              minimumSize: Size(30, 30),
-                                              tapTargetSize:
-                                                  MaterialTapTargetSize.padded,
-                                              alignment: Alignment.center),
-                                          child: Text('All'),
-                                        ),
-                                        IconButton(
-                                            icon: const Icon(
-                                                Icons.filter_list_outlined),
-                                            onPressed: () {
-                                              _showFilters(
-                                                  context, dataProvider);
-                                            }),
-                                      ],
-                                    ),
-                                  ),
-                                  if (dataProvider.filteredLocations?.isEmpty ==
-                                      true)
-                                    const Center(
-                                      child: Text('No result found!'),
-                                    )
-                                  else
-                                    Expanded(
-                                        child: ListView.builder(
-                                            itemCount: dataProvider
-                                                .filteredLocations?.length,
-                                            itemBuilder: (context, index) {
-                                              Location? location = (dataProvider
-                                                      .filteredLocations ??
-                                                  [])[index];
-                                              print("$location");
-                                              return Column(children: [
-                                                ListTile(
-                                                  title: Text(
-                                                    location?.location ?? "",
-                                                    style: GoogleFonts.poppins(
-                                                        fontSize: 18,
-                                                        color: Colors.black,
-                                                        fontWeight:
-                                                            FontWeight.w500),
-                                                  ),
-                                                  subtitle: Text(
-                                                    '${location?.geo} - ${location?.area}',
-                                                    style: GoogleFonts.poppins(
-                                                        fontSize: 14,
-                                                        color: Colors.black54,
-                                                        fontWeight:
-                                                            FontWeight.w500),
-                                                  ),
-                                                  trailing: const Icon(Icons
-                                                      .keyboard_arrow_right_rounded),
-                                                  onTap: () {
-                                                    Navigator.push(
-                                                        context,
-                                                        MaterialPageRoute(
-                                                            builder: (context) =>
-                                                                LocationDetailsPage(
-                                                                    location:
-                                                                        location!)));
-                                                  },
-                                                ),
-                                                const Divider()
-                                              ]);
-                                            })),
-                                ]))
-                            : const Center(child: Text('No data found!')))));
   }
 }
--- lib/screen/location_details_screen.dart
+++ lib/screen/location_details_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_assignment/data/models/location_response.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_assignment/screen/widgets/common_app_bar.dart';
 
 class LocationDetailsPage extends StatelessWidget {
   const LocationDetailsPage({super.key, required this.location});
@@ -10,53 +10,52 @@ class LocationDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text("TCS Locator",
-              style: GoogleFonts.poppins(
-                  color: Colors.white, fontWeight: FontWeight.w500)),
-          actions: [
-            Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 12, horizontal: 8),
-                child: Image.asset('assets/images/tcs_logo.png',
-                    fit: BoxFit.cover))
-          ],
+        appBar: const CommonAppBar(title: "TCS Locator"),
+        body: LocationDetailsPageBody(location: location));
+  }
+}
+
+class LocationDetailsPageBody extends StatelessWidget {
+  const LocationDetailsPageBody({super.key, required this.location});
+
+  final Location location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        ListTile(
+          title: const Text('TCS Center Name'),
+          subtitle: Text(location.location ?? ''),
+          trailing: const Icon(Icons.home_work_outlined),
         ),
-        body: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            ListTile(
-              title: const Text('TCS Center Name'),
-              subtitle: Text(location.location ?? ''),
-              trailing: const Icon(Icons.home_work_outlined),
-            ),
-            ListTile(
-              title: const Text('Location'),
-              subtitle: Text(
-                '${location.geo} - ${location.area}',
-              ),
-              trailing: const Icon(Icons.location_pin),
-            ),
-            ListTile(
-              title: Text('Phone'),
-              subtitle: Text(location.phone ?? 'N/A'),
-              trailing: Icon(Icons.phone),
-            ),
-            ListTile(
-              title: Text('Email'),
-              subtitle: Text(location.email ?? 'N/A'),
-              trailing: Icon(Icons.email),
-            ),
-            ListTile(
-              title: Text('Address'),
-              subtitle: Text(location.address ?? 'N/A'),
-            ),
-            ListTile(
-              title: Text('Office Type'),
-              subtitle: Text(location.officeType?.firstOrNull ?? 'N/A'),
-            ),
-          ],
-        ));
+        ListTile(
+          title: const Text('Location'),
+          subtitle: Text(
+            '${location.geo} - ${location.area}',
+          ),
+          trailing: const Icon(Icons.location_pin),
+        ),
+        ListTile(
+          title: Text('Phone'),
+          subtitle: Text(location.phone ?? 'N/A'),
+          trailing: Icon(Icons.phone),
+        ),
+        ListTile(
+          title: Text('Email'),
+          subtitle: Text(location.email ?? 'N/A'),
+          trailing: Icon(Icons.email),
+        ),
+        ListTile(
+          title: Text('Address'),
+          subtitle: Text(location.address ?? 'N/A'),
+        ),
+        ListTile(
+          title: Text('Office Type'),
+          subtitle: Text(location.officeType?.firstOrNull ?? 'N/A'),
+        ),
+      ],
+    );
   }
 }
