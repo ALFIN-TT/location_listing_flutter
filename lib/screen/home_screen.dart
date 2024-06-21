5c7c00fc6c052069e67b69ad95d5bf6420bfe39d
Alfin <alfinthumbanath@gmail.com>
2024-06-21 09:48:24 +0530

    ui and cleaned

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -34,8 +34,6 @@ class HomePageBody extends StatefulWidget {
 }
 
 class HomePageBodyState extends State<HomePageBody> {
-  TextEditingController searchTextEditingController = TextEditingController();
-
   @override
   Widget build(BuildContext context) {
     return Consumer<LocationProvider>(
@@ -50,109 +48,36 @@ class HomePageBodyState extends State<HomePageBody> {
                             padding: const EdgeInsets.only(
                                 right: 5, left: 5, top: 12, bottom: 12),
                             child: Column(children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Row(
-                                  children: <Widget>[
-                                    Expanded(
-                                      child: TextField(
-                                        controller: searchTextEditingController,
-                                        decoration: const InputDecoration(
-                                            contentPadding: EdgeInsets.only(
-                                                left: 5,
-                                                top: 12,
-                                                right: 2,
-                                                bottom: 12),
-                                            focusedBorder: OutlineInputBorder(
-                                              borderSide: BorderSide(
-                                                  color: Colors.black,
-                                                  width: 2.0),
-                                            ),
-                                            enabledBorder: OutlineInputBorder(
-                                              borderSide: BorderSide(
-                                                  color: Colors.black54,
-                                                  width: 2.0),
-                                            ),
-                                            hintText: 'Search here...'),
-                                        onChanged: (query) {
-                                          dataProvider.applySearchQuery(query);
-                                          dataProvider.fetchLocationsResult();
-                                        },
-                                      ),
-                                    ),
-                                    TextButton(
-                                      onPressed: () {
-                                        dataProvider
-                                            .applyFilter(locationFilterAll);
-                                        dataProvider.fetchLocationsResult();
-                                      },
-                                      style: TextButton.styleFrom(
-                                          padding: EdgeInsets.zero,
-                                          minimumSize: Size(30, 30),
-                                          tapTargetSize:
-                                              MaterialTapTargetSize.padded,
-                                          alignment: Alignment.center),
-                                      child: Text('All'),
-                                    ),
-                                    IconButton(
-                                        icon: const Icon(
-                                            Icons.filter_list_outlined),
-                                        onPressed: () {
-                                          _showFilters(context, dataProvider);
-                                        }),
-                                  ],
-                                ),
-                              ),
+                              SearchAndFilterRow(dataProvider: dataProvider),
                               if (dataProvider.filteredLocations?.isEmpty ==
                                   true)
-                                const Center(
+                                 const Expanded(
+                                    child: Center(
                                   child: Text('No result found!'),
-                                )
+                                ))
                               else
-                                Expanded(
-                                    child: ListView.builder(
-                                        itemCount: dataProvider
-                                            .filteredLocations?.length,
-                                        itemBuilder: (context, index) {
-                                          Location? location =
-                                              (dataProvider.filteredLocations ??
-                                                  [])[index];
-                                          return Column(children: [
-                                            ListTile(
-                                              title: Text(
-                                                location?.location ?? "",
-                                                style: GoogleFonts.poppins(
-                                                    fontSize: 18,
-                                                    color: Colors.black,
-                                                    fontWeight:
-                                                        FontWeight.w500),
-                                              ),
-                                              subtitle: Text(
-                                                '${location?.geo} - ${location?.area}',
-                                                style: GoogleFonts.poppins(
-                                                    fontSize: 14,
-                                                    color: Colors.black54,
-                                                    fontWeight:
-                                                        FontWeight.w500),
-                                              ),
-                                              trailing: const Icon(Icons
-                                                  .keyboard_arrow_right_rounded),
-                                              onTap: () {
-                                                Navigator.push(
-                                                    context,
-                                                    MaterialPageRoute(
-                                                        builder: (context) =>
-                                                            LocationDetailsPage(
-                                                                location:
-                                                                    location!)));
-                                              },
-                                            ),
-                                            const Divider()
-                                          ]);
-                                        })),
+                                LocationsList(dataProvider: dataProvider)
                             ]))
                         : const Center(child: Text('No data found!'))));
   }
+}
+
+class SearchAndFilterRow extends StatefulWidget {
+  final LocationProvider dataProvider;
+
+  SearchAndFilterRow({required this.dataProvider, super.key});
+
+  @override
+  State<StatefulWidget> createState() =>
+      SearchAndFilterRowState(dataProvider: dataProvider);
+}
+
+class SearchAndFilterRowState extends State<SearchAndFilterRow> {
+  final LocationProvider dataProvider;
+
+  SearchAndFilterRowState({required this.dataProvider});
+
+  TextEditingController searchTextEditingController = TextEditingController();
 
   Future<void> _showFilters(
       BuildContext context, LocationProvider dataProvider) async {
@@ -195,5 +120,96 @@ class HomePageBodyState extends State<HomePageBody> {
         ),
       ),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextField(
+              controller: searchTextEditingController,
+              decoration: const InputDecoration(
+                  contentPadding:
+                      EdgeInsets.only(left: 12, top: 12, right: 2, bottom: 12),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black, width: 2.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black54, width: 2.0),
+                  ),
+                  hintText: 'Search here...'),
+              onChanged: (query) {
+                dataProvider.applySearchQuery(query);
+                dataProvider.fetchLocationsResult();
+              },
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              dataProvider.applyFilter(locationFilterAll);
+              dataProvider.fetchLocationsResult();
+            },
+            style: TextButton.styleFrom(
+                padding: EdgeInsets.zero,
+                minimumSize: Size(30, 30),
+                tapTargetSize: MaterialTapTargetSize.padded,
+                alignment: Alignment.center),
+            child: Text('All'),
+          ),
+          IconButton(
+              icon: const Icon(Icons.filter_list_outlined),
+              onPressed: () {
+                _showFilters(context, dataProvider);
+              }),
+        ],
+      ),
+    );
+  }
+}
+
+class LocationsList extends StatelessWidget {
+  final LocationProvider dataProvider;
+
+  LocationsList({required this.dataProvider, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: ListView.builder(
+            itemCount: dataProvider.filteredLocations?.length,
+            itemBuilder: (context, index) {
+              Location? location =
+                  (dataProvider.filteredLocations ?? [])[index];
+              return Column(children: [
+                ListTile(
+                  title: Text(
+                    location?.location ?? "",
+                    style: GoogleFonts.poppins(
+                        fontSize: 18,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  subtitle: Text(
+                    '${location?.geo} - ${location?.area}',
+                    style: GoogleFonts.poppins(
+                        fontSize: 14,
+                        color: Colors.black54,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  trailing: const Icon(Icons.keyboard_arrow_right_rounded),
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                LocationDetailsPage(location: location!)));
+                  },
+                ),
+                const Divider()
+              ]);
+            }));
   }
 }
